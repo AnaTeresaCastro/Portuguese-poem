5964852d1fe62d3debc0019fe11e70c8f6a7564b
AnaTeresaCastro <ana.1148@hotmail.com>
2024-10-03 15:44:54 +0100

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  let searchInput = document.querySelector("#search-input");
+  let apiKey = "41f9b9t0ec755bae06caf1o002842139";
+  let context =
+    "you are a creative and romantic poem writer. Please write poems with 4 lines. Poems must be in the portuguese language, and sign 'SheCodes AI' in bold at the end of the lines. ";
+  let prompt = `Please write a poem using the word ${searchInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
+}
+function showPoem(response) {
   new Typewriter("#poem", {
-    strings: "bla bla bla bla bla",
+    strings: response.data.answer,
     autoStart: true,
+    delay: 1,
     cursor: "",
   });
 }
