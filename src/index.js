c3321ad4f4a31cbbb3afa18fb47301d0c2fc6d18
AnaTeresaCastro <ana.1148@hotmail.com>
2024-10-04 12:57:16 +0100

    Improved UX

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ function generatePoem(event) {
   let searchInput = document.querySelector("#search-input");
   let apiKey = "41f9b9t0ec755bae06caf1o002842139";
   let context =
-    "you are a creative and romantic poem writer. Please write poems with 4 lines. Poems must be in the portuguese language, and sign 'SheCodes AI' in bold at the end of the lines. ";
+    "you are a creative and romantic poem writer. Please write poems with 4 lines and separate each line with a <br />. Poems must be in the portuguese language, and sign 'SheCodes AI' inside a strong element at the end of the lines. ";
   let prompt = `Please write a poem using the word ${searchInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showPoem);
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">⏳ Generating a poem about ${searchInput.value} </div>`;
 }
 function showPoem(response) {
   new Typewriter("#poem", {
